5b2cc0f39b8b62d6c25bc65275849766c08191d2
dhruvil-codes <22ecdhr063@ldce.ac.in>
2023-11-04 11:49:01 +0530

    fix:pwa

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@
  */
 await import("./src/env.mjs");
 
+import withPWA from "next-pwa";
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: false,
@@ -34,11 +36,11 @@ const config = {
   },
 };
 
-const withPWA = require("next-pwa")({
+const withPWAExtra = withPWA({
   dest: "public", // Destination directory for the PWA files
   disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
 });
 
-export default withPWA(config);
+export default withPWAExtra(config);
