f2ae39ed63810a90c16c1ed5c6955daab4e463d5
dhruvil-codes <22ecdhr063@ldce.ac.in>
2023-11-04 11:56:19 +0530

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,20 +16,23 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-misused-promises": [
-      2,
-      {
-        checksVoidReturn: { attributes: false },
-      },
-    ],
+    // "@typescript-eslint/consistent-type-imports": [
+    //   "warn",
+    //   {
+    //     prefer: "type-imports",
+    //     fixStyle: "inline-type-imports",
+    //   },
+    // ],
+    "@typescript-eslint/consistent-type-imports": "off",
+    // "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": "off",
+    // "@typescript-eslint/no-misused-promises": [
+    //   2,
+    //   {
+    //     checksVoidReturn: { attributes: false },
+    //   },
+    // ],
+    "@typescript-eslint/no-misused-promises": "off",
   },
 };
 
